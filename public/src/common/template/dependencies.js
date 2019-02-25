4d2138f0ee9bed069f060fab2f3d8087d4c34338
Caique Ferreira <caiquestup@gmail.com>
2019-02-25 08:40:53 -0400

    Resolvido problema Jquery -- console

---
+++ public/src/common/template/dependencies.js
@@ -0,0 +1,17 @@
+global.jQuery = require('../../../../node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
+window.$ = window.jQuery = jQuery;
+ 
+require('modules/admin-lte/plugins/jQueryUI/jquery-ui.min');
+require('modules/admin-lte/bootstrap/js/bootstrap.min');
+require('modules/admin-lte/plugins/fastclick/fastclick');
+require('modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min');
+require('modules/admin-lte/dist/js/app');
+ 
+require('modules/font-awesome/css/font-awesome.min.css');
+require('modules/ionicons/dist/css/ionicons.css');
+require('modules/admin-lte/bootstrap/css/bootstrap.min.css');
+require('modules/admin-lte/dist/css/AdminLTE.css');
+require('modules/admin-lte/dist/css/skins/_all-skins.css');
+require('modules/admin-lte/plugins/iCheck/flat/blue.css');
+ 
+import './custom.css'
